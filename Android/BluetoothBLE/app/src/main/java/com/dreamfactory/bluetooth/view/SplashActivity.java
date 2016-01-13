1ea8568b09161798637c553b017fffb9e34e128c
kurtis_hu@hotmail.com <kurtis_hu@hotmail.com>
2016-01-13 22:26:05 +0800

    Add AwesomeSplash lib

--- Android/BluetoothBLE/app/src/main/java/com/dreamfactory/bluetooth/view/SplashActivity.java
+++ Android/BluetoothBLE/app/src/main/java/com/dreamfactory/bluetooth/view/SplashActivity.java
@@ -1,6 +1,7 @@
 package com.dreamfactory.bluetooth.view;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -8,20 +9,37 @@ import android.os.Handler;
 import android.view.MotionEvent;
 import android.view.View;
 
+import com.daimajia.androidanimations.library.Techniques;
 import com.dreamfactory.bluetooth.R;
+import com.viksaa.sssplash.lib.activity.AwesomeSplash;
+import com.viksaa.sssplash.lib.cnst.Flags;
+import com.viksaa.sssplash.lib.model.ConfigSplash;
 
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
  * status bar and navigation/system bar) with user interaction.
  */
-public class SplashActivity extends AppCompatActivity {
+public class SplashActivity extends AwesomeSplash {
+
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    public void initSplash(ConfigSplash configSplash) {
+        //Customize Circular Reveal
+        configSplash.setBackgroundColor(R.color.colorPrimary); //any color you want form colors.xml
+        configSplash.setAnimCircularRevealDuration(2000); //int ms
+        configSplash.setRevealFlagX(Flags.REVEAL_RIGHT);  //or Flags.REVEAL_LEFT
+        configSplash.setRevealFlagY(Flags.REVEAL_BOTTOM); //or Flags.REVEAL_TOP
 
-        setContentView(R.layout.activity_splash);
-
+        configSplash.setLogoSplash(R.mipmap.ic_launcher); //or any other drawable
+        configSplash.setAnimLogoSplashDuration(2000); //int ms
+        configSplash.setAnimLogoSplashTechnique(Techniques.Bounce);
     }
 
+
+    @Override
+    public void animationsFinished() {
+
+        startActivity(new Intent(SplashActivity.this, MainActivity.class));
+        finish();
+    }
 }
