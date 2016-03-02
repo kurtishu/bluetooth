818410b155c782967d768ac6de53870b400c7a5f
Kurtis Hu <kurtis_hu@hotmail.com>
2016-03-02 16:44:01 +0800

    Update

--- Android/BluetoothBLE/app/src/main/java/com/dreamfactory/bluetooth/view/fragment/ConfigSettingFragment.java
+++ Android/BluetoothBLE/app/src/main/java/com/dreamfactory/bluetooth/view/fragment/ConfigSettingFragment.java
@@ -5,10 +5,26 @@ import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 import com.dreamfactory.bluetooth.R;
 
 public class ConfigSettingFragment extends Fragment {
+
+    private EditText etTimingSetting;
+    private EditText etStartTimingHour;
+    private EditText etStartTimingMis;
+    private EditText etEndTimingHour;
+    private EditText etEndTimingMis;
+    private EditText etInflatedTime;
+    private EditText etDeflatedTime;
+    private EditText etWorkingThreshold;
+    private EditText etDegree;
+    private EditText etWokingTime;
+    private EditText etRestDevice;
+    private EditText etResetData;
+    private EditText etClearData;
+
 
     public ConfigSettingFragment() {
         // Required empty public constructor
@@ -27,6 +43,7 @@ public class ConfigSettingFragment extends Fragment {
         return fragment;
     }
 
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,7 +55,25 @@ public class ConfigSettingFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_setting, container, false);
+        View view = inflater.inflate(R.layout.fragment_setting, container, false);
+        initViews(view);
+        return view;
+    }
+
+    private void initViews(View view) {
+        etTimingSetting = (EditText) view.findViewById(R.id.timming_setting);
+        etStartTimingHour = (EditText) view.findViewById(R.id.start_timming_h);
+        etStartTimingMis = (EditText) view.findViewById(R.id.start_timming_m);
+        etEndTimingHour = (EditText) view.findViewById(R.id.end_timming_h);
+        etEndTimingMis = (EditText) view.findViewById(R.id.end_timming_m);
+        etInflatedTime = (EditText) view.findViewById(R.id.inflated_time);
+        etDeflatedTime = (EditText) view.findViewById(R.id.deflate_time);
+        etWorkingThreshold = (EditText) view.findViewById(R.id.working_threshold);
+        etWokingTime = (EditText) view.findViewById(R.id.woking_time);
+        etRestDevice = (EditText) view.findViewById(R.id.reset);
+        etResetData = (EditText) view.findViewById(R.id.reset_data);
+        etDegree = (EditText) view.findViewById(R.id.degree);
+        etClearData = (EditText) view.findViewById(R.id.clear_data);
     }
 
 }
